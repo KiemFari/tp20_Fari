e3ffeb7b469f702f7b5cd0646bd52ed6a031a357
Kiemtore_Fariratou <kiemtorefariratou@.com>
2023-08-11 13:56:31 +0100

    master

--- index.js
+++ index.js
@@ -1,66 +1,119 @@
- let myForm = document.getElementById('myForm');
+const loi =document.getElementById("loi")
+const prenom =document.getElementById("prenom");
+const nom =document.getElementById("nom");
+const email =document.getElementById("email");
+const phoneNumber =document.getElementById("number");
+const age =document.getElementById("age");
+const country =document.getElementById("country");
 
-myForm.addEventListener("submit", function(e) {
-// e.preventDefault();
-let myInput = document.getElementById("all");
-let myAlles = document.getElementById("alles")
-let myRegex =  /^[toute les champs doivent etre rempli]$/;
-if (myInput.value.trim() === "" ) {
-let myError = document.getElementById("error")
-myError.innerHTML = "Le champs  est vide";
-myError.style.color = "red";
-//   
-   e.preventDefault(); 
+const  bonde =document.getElementById("bonde");
+ const  rajout =document.getElementById("rajout");
+ const rajout1 =document.getElementById("rajout1");
+ const  rajout2 =document.getElementById("rajout2");
+ const  rajout3 =document.getElementById("rajout3");
+ const rajout4 =document.getElementById("rajout4");
+ const  rajout5 =document.getElementById("rajout5");
+//  const  rajout6 =document.getElementById("rajout6");
+//  const  rajout7 =document.getElementById("rajout7");
 
-} else if (myRegex.test(myInput.value)=== false) {
-   let myError = document.getElementById("error")
-   myInput.innerHTML = "toutes les champs doivent etre rempli avant la validation";
-   myInput.style.color = "red"
-}
+ 
 
-});
+ bonde.addEventListener('click', function(e){
+   if (!prenom.value) {
+      rajout.innerHTML='remplir';
+      rajout.style.color="red"
+      prenom.style.background="red";
+      e.preventDefault()
+   }else{
+      rajout.innerHTML=""
+      prenom.style.background="green"
+   }
+   
+ });
+ bonde.addEventListener('click', function(e){
+   if (!nom.value) {
+      rajout1.innerHTML='remplir';
+      rajout1.style.color="red"
+      nom.style.background="red";
+      e.preventDefault()
+   }else{
+      rajout1.innerHTML=""
+      nom.style.background="green"
+   }
+   
+ });
+ bonde.addEventListener('click', function(e){
+   if (!email.value) {
+      rajout2.innerHTML='remplir';
+      rajout2.style.color="red"
+      email.style.background="red";
+      e.preventDefault()
+   }else{
+      rajout2.innerHTML=""
+      email.style.background="green"
+   }
+   
+ });
+ bonde.addEventListener('click', function(e){
+   if (!phoneNumber.value) {
+      rajout3.innerHTML='remplir';
+      rajout3.style.color="red"
+     phoneNumber.style.background="red";
+      e.preventDefault()
+   }else{
+      rajout3.innerHTML=""
+      .style.background="green"
+   }
+   
+ });
+//  bonde.addEventListener('click', function(e){
+//    if (!phoneNumber.value) {
+//       rajout4.innerHTML='';
+//       rajout4.style.color="red"
+//      phoneNumber .style.background="red";
+//       e.preventDefault()
+//    }else{
+//       rajout4.innerHTML=""
+//       .style.background="green"
+//    }
+   
+//  });
+//  bonde.addEventListener('click', function(e){
+//    if (!phoneNumber.value) {
+//       rajout5.innerHTML='';
+//       rajout5.style.color="red"
+//      phoneNumber .style.background="red";
+//       e.preventDefault()
+//    }else{
+//       rajout5.innerHTML=""
+//       .style.background="green"
+//    }
+   
+//  });
+ bonde.addEventListener('click', function(e){
+   if (!age.value) {
+      rajout4.innerHTML='remplir';
+      rajout4.style.color="red"
+      age.style.background="red";
+      e.preventDefault()
+   }else{
+      rajout4.innerHTML=""
+      age.style.background="green"
+   }
+   
+ });
+ bonde.addEventListener('click', function(e){
+   if (!country.value) {
+      rajout5.innerHTML='remplir';
+      rajout5.style.color="red"
+      country.style.background="red";
+      e.preventDefault()
+   }else{
+      rajout5.innerHTML=""
+      country.style.background="green"
+   }
+   
+ });
+   
+ 
 
-// document.getElementById("inscription").addEventListener("submit", function(e) {
-// e.preventDefault();
-// var error;
-// var First = document.getElementById("First");
-// var Last   = document.getElementById("Last");
-// var Email  = document.getElementById("Email");
-// var Number = document.getElementById("Number");
-// var Age= document.getElementById("Age");
-// var Male = document.getElementById("Male");
-// var Female = document.getElementById("Female");
-// var yourCountry = document.getElementById("Country");
-
-// if (! yourCountry.value) {
-//    error = "Le champs de country est vide";
-// }
-// if (!Age.value) {
-//    error = "Le champs de Age est vide";
-// }
-// if (!Number.value) {
-//    error = "Le champs de Number est vide";
-// }
-// if (!Email.value) {
-//    error= "Le champs de Email est vide";
-// }
-// if (!Last.value) {
-//    error = "Le champs de Last est vide";
-// }
-// if (!First.value) {
-//    error = "Le champs de First est vide";
-// }
-// if (!Male.value) {
-//    error = "Le champs de Male est vide";
-// }
-// if (!Female.value) {
-//    error = "Le champs de Female est vide";
-// }
-// if(error){
-//    e.preventDefault();
-//    document.getElementById("error").innerHTML = error
-//    return false;
-// }else{
-//    alert('formulaire soumis avec success!');
-// }
-// });
